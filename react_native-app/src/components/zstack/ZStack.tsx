a4f896cf83fa68f0a9566cfee3f7df5215f488ea
Kirill2690 <kirillmuravev2690@gmail.com>
2023-04-13 12:04:52 +0300

    add:refactoring ZStack

--- react_native-app/src/components/zstack/ZStack.tsx
+++ react_native-app/src/components/zstack/ZStack.tsx
@@ -1,15 +1,29 @@
-import {ReactNode} from "react";
+import React, {ReactNode} from "react";
 import {View} from "react-native";
 
-interface ZStackPropsType{
-    children:JSX.Element[]|JSX.Element,
-    reverse?:boolean
+interface ZStackPropsType {
+    children: JSX.Element[] | JSX.Element,
+    reverse?: boolean
 }
 
-export const ZStack=({children}:ZStackPropsType)=>{
-    return(
-        <View>
-            {children}
+//create function ---> add every children new
+// property-absolute
+
+const getAbsoluteChuldren = (children: JSX.Element[] | JSX.Element) => {
+    let childrenArray = React.Children.toArray(children)
+    return childrenArray.map((child: any) => {
+        return React.cloneElement(
+            child, {position: 'absolute'},
+            child.props.children
+        )
+    })
+}
+
+
+export const ZStack = ({children}: ZStackPropsType) => {
+    return (
+        <View style={{position:'relative'}}>
+            {getAbsoluteChuldren(children)}
         </View>
     )
 }
